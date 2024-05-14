c2d7c310462b0258e1bdd63750e54bc85bde876e
joel-smith <joel.craig.smith@gmail.com>
2024-05-14 15:30:33 -0400

    navigation link

--- resources/js/Pages/Polls/Index.vue
+++ resources/js/Pages/Polls/Index.vue
@@ -12,7 +12,8 @@ const props = defineProps({ polls: Object })
         <InertiaLink :href="route('polls.show', poll.id)" class="text-blue-500 hover:text-blue-700">{{ poll.title }}</InertiaLink>
       </li>
     </ul>
-  </div>
+
+    <InertiaLink :href="route('dashboard')" class="inline-block px-4 py-2 text-sm font-semibold leading-none text-blue-500 transition-colors duration-200 bg-transparent border border-blue-500 rounded hover:bg-blue-500 hover:text-white hover:border-transparent hover:shadow-md hover:ring-2 hover:ring-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-blue-200">Return to Dashboard</InertiaLink>  </div>
 </template>
 
 <style scoped>
